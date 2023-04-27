a97b8e6281a34ca25981c8f5339581b784737419
JiSuMun <mjs3827@naver.com>
2023-04-27 13:49:39 +0900

    add posts-backend

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf import settings
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
-# Create your models here.
+class Post(models.Model):
+    CAFE = 'cafe'
+    BAKERY = 'bakery'
+    CATEGORY_CHOICES = [
+        (CAFE, 'Cafe'),
+        (BAKERY, 'Bakery'),
+    ]
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_posts')
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = ProcessedImageField(blank=True, null=True, processors=[ResizeToFill(100, 100)], options={'quality':90})
+
+
+class Review(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    image = ProcessedImageField(blank=True, null=True, processors=[ResizeToFill(100, 100)], options={'quality':90})
+    DELICIOUS = 'DL'
+    OKAY = 'OK'
+    NOT_GOOD = 'NG'
+    EMOTIONS_CHOICES = [
+        (DELICIOUS, 'Delicious'),
+        (OKAY, 'Okay'),
+        (NOT_GOOD, 'Not good'),
+    ]
+    emotion = models.CharField(max_length=2, choices=EMOTIONS_CHOICES)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,82 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post, Review
+from .forms import PostForm, ReviewForm
 
-# Create your views here.
+
+def main(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'posts/main.html', context)
+
+
+# @login_required
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('posts:detail', post.pk)
+    else:
+        form = PostForm()
+    context = {'form': form}
+    return render(request, 'posts/create.html', context)
+
+
+def detail(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    review_form = ReviewForm()
+    reviews = post.review_set.all()
+    context = {
+        'post': post,
+        'review_form': review_form,
+        'reviews': reviews,
+        'emotions_choices': Review.EMOTIONS_CHOICES,
+    }
+    return render(request, 'posts/detail.html', context)
+
+
+# @login_required
+def delete(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    if request.user == post.user:
+        post.delete()
+    return redirect('posts:index')
+
+
+# @login_required
+def review_create(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    review_form = ReviewForm(request.POST, request.FILES)
+    if review_form.is_valid():
+        review = review_form.save(commit=False)
+        review.post = post
+        review.user = request.user
+        review_form.save()
+        return redirect('posts:detail', post.pk)
+    context = {
+        'post': post,
+        'review_form': review_form,
+    }
+    return render(request, 'posts/detail.html', context)
+
+
+# @login_required
+def review_delete(request, post_pk, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    if request.user == review.user:
+        review.delete()
+
+    return redirect('posts:detail', post_pk)
+
+
+# @login_required
+def likes(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    if request.user in post.like_users.all():
+        post.like_users.remove(request.user)
+    else:
+        post.like_users.add(request.user)
+    return redirect('posts:main')
