6a63fd9466cab452abf41baa4a4f44f9cbeb8821
JiSuMun <mjs3827@naver.com>
2023-05-06 02:05:11 +0900

    cloud

--- mango/settings.py
+++ mango/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://port-0-cafe-bakery-info-687p2alharv4ez.sel4.cloudtype.app/','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['port-0-cafe-bakery-info-687p2alharv4ez.sel4.cloudtype.app','localhost', '127.0.0.1']
 
 
 # Application definition
